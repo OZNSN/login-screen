330e46aa21b344e6e5b5085f0b8461a38102b4fa
Ozan Şen <104592478+OZNSN@users.noreply.github.com>
2022-06-30 16:14:12 +0300

    useRef kullanılarak ınput degerlerı console'a bastırılıyor

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useRef } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -12,16 +12,24 @@ import { AccountContext } from "./accountContext";
 
 export function LoginForm(props) {
   const { switchToSignup } = useContext(AccountContext);
+  const pwRef = useRef(null)
+  const mailRef = useRef(null)
+
+  function writeConsole() {
+    const password = pwRef.current?.value
+    const email = mailRef.current?.value
+    console.log(`Mail: ${email}\n Password: ${password}`)
+  }
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="user_mail" placeholder="Email" />
-        <Input type="password" name="password" placeholder="Parola"></Input>
+        <Input ref={mailRef} type="user_mail" placeholder="Email" />
+        <Input ref={pwRef} type="password" name="password" placeholder="Parola"></Input>
       </FormContainer>
       <MutedLink href="#">Şifrenizi mi unuttunuz?</MutedLink>
       <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit">Giriş yap</SubmitButton>
+      <SubmitButton type="submit" onClick={writeConsole}>Giriş yap</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Henüz bir hesabın yok mu?{" "}
--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useRef } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -12,17 +12,30 @@ import { AccountContext } from "./accountContext";
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
+  const nameRef = useRef(null)
+  const cnameRef = useRef(null)
+  const pwRef = useRef(null)
+  const mailRef = useRef(null)
+
+  function writeConsole() {
+    const name = nameRef.current?.value
+    const company_name = cnameRef.current?.value
+    const email = mailRef.current?.value
+    const password = pwRef.current?.value
+    
+    console.log(`Ad: ${name}\n Şirket Adı: ${company_name}\n Mail: ${email}\n Password: ${password}`)
+  }
 
   return (
     <BoxContainer>
       <FormContainer>
-       <Input type="name" placeholder="Kullanıcı Adı" />
-       <Input type="company_name" placeholder="Şirket Adı" />
-       <Input type="user_mail" placeholder="Email" />
-       <Input type="password" name="password" placeholder="Parola"></Input>
+       <Input ref={nameRef}type="name" placeholder="Kullanıcı Adı" />
+       <Input ref={cnameRef} type="company_name" placeholder="Şirket Adı" />
+       <Input ref={mailRef} type="user_mail" placeholder="Email" />
+       <Input ref={pwRef} type="password" name="password" placeholder="Parola"></Input>
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Kaydol</SubmitButton>
+      <SubmitButton type="submit" onClick={writeConsole}>Kaydol</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Zaten bir hesabın var mı?
