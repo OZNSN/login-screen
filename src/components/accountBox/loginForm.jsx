e1f6f99f5224ff63816dadcf4d89a090450e3f2e
Ozan Şen <104592478+OZNSN@users.noreply.github.com>
2022-07-07 11:29:04 +0300

    bug fixes

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useRef,useState } from "react";
+import React, { useContext,useEffect,useRef,useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -15,9 +15,41 @@ export function LoginForm(props) {
 
   const initialValues = {email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
   const handleChange = (e) => {
-    console.log(e.target); 
+    const { email, value } = e.target;
+    setFormValues({ ...formValues, [email]: value });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      errors.email = "Bu alan boş kalamaz";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Lütfen geçerli bir mail giriniz";
+    }
+    if (!values.password) {
+      errors.password = "Lütfen şifrenizi girin";
+    } else if (values.password.length < 8) {
+      errors.password = "Şifre 8 haneden küçük olamaz";
+    } 
+    return errors;
+ };
+
 
 
   const pwRef = useRef(null)
@@ -32,12 +64,14 @@ export function LoginForm(props) {
   return (
     <BoxContainer>
       <FormContainer>
-        <Input ref={mailRef} type="user_mail" placeholder="Email" value={ formValues.email} onChange= {handleChange} />
+        <Input ref={mailRef} type="text" placeholder="Email" value={ formValues.email} onChange= {handleChange} />
+        <p>{formErrors.email}</p>
         <Input ref={pwRef} type="password" name="password" placeholder="Parola" value={ formValues.password} onChange= {handleChange}></Input>
+        <p>{formErrors.password}</p>
       </FormContainer>
       <MutedLink href="#">Şifrenizi mi unuttunuz?</MutedLink>
       <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit" onClick={writeConsole}>Giriş yap</SubmitButton>
+      <SubmitButton type="submit" onClick={handleSubmit}>Giriş yap</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Henüz bir hesabın yok mu?{" "}
