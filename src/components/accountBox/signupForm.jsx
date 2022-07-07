0143e1c82a8f3748a7229fdb71a15659d774ca9e
Ozan Şen <104592478+OZNSN@users.noreply.github.com>
2022-07-07 11:11:25 +0300

    eksik kalan kısımlar tamamlanarak validasyon saglandı

--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -1,8 +1,7 @@
-import React, { useContext,useRef,useState } from "react";
+import React, { useContext,useEffect,useRef,useState } from "react";
 import {
   BoldLink,
   BoxContainer,
-  FormContainer,
   Input,
   MutedLink,
   SubmitButton,
@@ -13,19 +12,46 @@ import { AccountContext } from "./accountContext";
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
 
-  const initialValues = {name: "", company_name: "", email: "", password: "" };
+  const initialValues = { name: "",company_name: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFormValues({...formValues, [name]: value});
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
 
-  }
-
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.name) {
+      errors.name = "Bu alan boş kalamaz";
+    }
+    if (!values.email) {
+      errors.email = "Bu alan boş kalamaz";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Lütfen geçerli bir mail giriniz";
+    }
+    if (!values.password) {
+      errors.password = "Lütfen şifrenizi girin";
+    } else if (values.password.length < 8) {
+      errors.password = "Şifre 8 haneden küçük olamaz";
+    } 
+    return errors;
+ };
 
   const nameRef = useRef(null)
   const cnameRef = useRef(null)
@@ -45,15 +71,15 @@ export function SignupForm(props) {
 
   return (
     <BoxContainer>
-      <pre>{ JSON.stringify(formValues, undefined, 2)}</pre>
-      <FormContainer>
        <Input ref={nameRef}type="text" name="name" placeholder="Kullanıcı Adı" value={ formValues.name} onChange= {handleChange} />
+       <p>{formErrors.name}</p>
        <Input ref={cnameRef} type="text" name="company_name" placeholder="Şirket Adı" value={ formValues.company_name} onChange= {handleChange} />
        <Input ref={mailRef} type="text" name="email" placeholder="Email" value={ formValues.email} onChange= {handleChange} />
+       <p>{formErrors.email}</p>
        <Input ref={pwRef} type="password" name="password" placeholder="Parola"value={ formValues.password} onChange= {handleChange}></Input>
-      </FormContainer>
+       <p>{formErrors.password}</p>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit" onSubmit={handleSubmit} onClick={writeConsole}>Kaydol</SubmitButton>
+      <SubmitButton type="submit"  onClick={handleSubmit} >Kaydol</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Zaten bir hesabın var mı?
